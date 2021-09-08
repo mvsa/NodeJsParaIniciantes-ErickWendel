876cef8a98ca2ba2ff1f11cb0678200e077899a1
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-09-07 21:15:34 -0300

    refactoring using async/await

---
+++ 02.2-Refactor-Async-await/index.js
@@ -0,0 +1,87 @@
+
+const util = require ('util');
+
+const getUserAddressAsync = util.promisify(getUserAddress); 
+
+
+function getUser(){
+    return new Promise(function resolvePromise(resolve, reject){
+        setTimeout(function (){
+            return resolve({
+                id:1,
+                nome: 'Aladin',
+                birthday: new Date()
+            })
+        },1000)
+    })
+}
+
+
+function getUserPhone(idUser){
+    return new Promise(function resolvePromise(resolve,reject){
+        setTimeout(()=>{
+            return resolve({ 
+                number:1111111111,
+                ddd: 11
+            })
+        },2000)
+    })
+}
+
+
+
+function getUserAddress(idUser, callback){
+    setTimeout(()=>{
+        return callback (null, {
+            rua: 'rua azul',
+            numero: 0
+        })
+    },2000);
+
+}
+
+main()
+
+//adicionando um async ele automaticamente vai retonar uma promisse
+async function main() {
+    try{
+        console.time('medida-promise');
+        const usuario = await getUser();
+
+
+        // const telefone = await getUserPhone(usuario.id);
+        // const endereco = await getUserAddressAsync(usuario.id);
+        //mudança siginificativa de performance ao colocar os awaits comentados no bloco de promise.all
+
+
+        //esses dois podem vir junto no bloco abaixo pois dependem apenas do codigo de usuario que nesse momento 
+        //ja foi coletado
+        const resultado = await Promise.all([
+            getUserPhone(usuario.id),
+            getUserAddressAsync(usuario.id)
+        ]);
+
+        const endereco = resultado[1];
+        const telefone = resultado[0];
+
+        console.log(`
+        Nome: ${usuario.nome}
+        Endereço: ${endereco.rua}, ${endereco.numero}
+        Telefone: ${telefone.number}
+        `);
+        console.timeEnd('medida-promise');
+
+    }catch(error){
+        console.error('erro', error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
