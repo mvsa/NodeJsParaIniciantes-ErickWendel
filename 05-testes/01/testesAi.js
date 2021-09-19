87d366e214d3ad26bc219d3dd03293406a90001e
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-09-18 23:21:40 -0300

    feat(module 05): Setting up tests

---
+++ 05-testes/01/testesAi.js
@@ -0,0 +1,12 @@
+const assert = require('assert'); // modulo do node para asserções
+
+//assert.ok(false)
+
+describe('Star Wats Tests', ()=>{
+    it('should be able to get r2d2 with correct format', async ()=>{
+        const expected = [{nome: 'R2-D2', peso: '96'}]
+        const nomeBase = 'r2-d2'
+
+        
+    })
+})
