19b3060f3cd38be4ef8aae4e2cd622baea573bc0
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-10-13 14:59:11 -0300

    finishing crud

--- 09-multiDB-postegre-mongo/src/db/strategies/mongodb.js
+++ 09-multiDB-postegre-mongo/src/db/strategies/mongodb.js
@@ -2,75 +2,84 @@ const ICrud = require("./interfaces/InterfaceCrud");
 const Mongoose = require("mongoose");
 
 const STATUS = {
-  0: "Desconectado",
-  1: "Conectado",
-  2: "Conectando",
-  3: "Desconectando",
+    0: "Desconectado",
+    1: "Conectado",
+    2: "Conectando",
+    3: "Desconectando",
 };
 
 class MongoDB extends ICrud {
-  constructor() {
-    super(); // classe que extende precisa chamar o super para invocar o construtor da pai
-    this._herois = null;
-    this._driver = null;
-  }
+    constructor() {
+        super(); // classe que extende precisa chamar o super para invocar o construtor da pai
+        this._herois = null;
+        this._driver = null;
+    }
 
-  isConnected() {
-    const state = STATUS[this._driver.readyState];
-    if (state === STATUS[1]) return state;
-    if (state !== STATUS[2]) return state;
+    isConnected() {
+        const state = STATUS[this._driver.readyState];
+        if (state === STATUS[1]) return state;
+        if (state !== STATUS[2]) return state;
 
-    new Promise((resolve) => setTimeout(resolve, 1000));
-    return STATUS[this._driver.readyState];
-  }
+        new Promise((resolve) => setTimeout(resolve, 1000));
+        return STATUS[this._driver.readyState];
+    }
 
-  defineModel() {
-    const heroiSchema = new Mongoose.Schema({
-      nome: {
-        type: String,
-        required: true,
-      },
-      poder: {
-        type: String,
-        required: true,
-      },
-      insertedAt: {
-        type: Date,
-        default: new Date(),
-      },
-    });
+    defineModel() {
+        const heroiSchema = new Mongoose.Schema({
+            nome: {
+                type: String,
+                required: true,
+            },
+            poder: {
+                type: String,
+                required: true,
+            },
+            insertedAt: {
+                type: Date,
+                default: new Date(),
+            },
+        });
 
-    this._herois = Mongoose.models.herois || Mongoose.model("herois", heroiSchema);
-  }
+        this._herois =
+            Mongoose.models.herois || Mongoose.model("herois", heroiSchema);
+    }
 
-  connect() {
-    Mongoose.connect(
-      "mongodb://marcos:minhasenha@localhost:27017/herois",
-      {
-        useNewUrlParser: true,
-      },
-      (error) => {
-        if (!error) return;
-        console.log("Falha na conexão", error);
-      }
-    );
+    connect() {
+        Mongoose.connect(
+            "mongodb://marcos:minhasenha@localhost:27017/herois",
+            {
+                useNewUrlParser: true,
+            },
+            (error) => {
+                if (!error) return;
+                console.log("Falha na conexão", error);
+            }
+        );
 
-    const connection = Mongoose.connection;
-    connection.once("open", () => {
-      console.log("database rodando");
-    });
+        const connection = Mongoose.connection;
+        connection.once("open", () => {
+            console.log("database rodando");
+        });
 
-    this._driver = connection
-    this.defineModel()
-  }
+        this._driver = connection;
+        this.defineModel();
+    }
 
-  async create(item) {
-    return await this._herois.create(item);
-  }
+    async create(item) {
+        return await this._herois.create(item);
+    }
 
-  async read(item, skip=0, limit=10) {
-    return await this._herois.find(item).skip(skip).limit(limit);
-  }
+    async read(item, skip = 0, limit = 10) {
+        return await this._herois.find(item).skip(skip).limit(limit);
+    }
+
+    update(id, item) {
+      return this._herois.updateOne({_id:id}, {$set:item})
+    }
+
+    delete(id){
+      return this._herois.deleteOne({_id:id})
+    }
 }
 
 module.exports = MongoDB;
--- 09-multiDB-postegre-mongo/src/tests/mongoDbStrategy.test.js
+++ 09-multiDB-postegre-mongo/src/tests/mongoDbStrategy.test.js
@@ -3,20 +3,28 @@ const MongoDb = require("../db/strategies/mongodb");
 const Context = require("../db/strategies/base/contextStrategy");
 
 const MOCK_HEROI_CADASTRAR = {
-  nome:'HulkS',
-  poder:'força'
-}
+    nome: "HulkS",
+    poder: "força",
+};
 
 const MOCK_HEROI_DEFAULT = {
-  nome:`homemaranha-${Date.now()}`,
-  poder:'teia'
-}
+    nome: `homemaranha-${Date.now()}`,
+    poder: "teia",
+};
+
+const MOCK_HEROI_UPDATE = {
+    nome: `pato-${Date.now()}`,
+    poder: "velocidade",
+};
+let MOCK_HEROI_ID = "";
 const context = new Context(new MongoDb());
 
-describe("MongoDb test suite", function() {
+describe("MongoDb test suite", function () {
     this.beforeAll(async () => {
         await context.connect();
-        await context.create(MOCK_HEROI_DEFAULT)
+        await context.create(MOCK_HEROI_DEFAULT);
+        const result = await context.create(MOCK_HEROI_UPDATE);
+        MOCK_HEROI_ID = result._id;
     });
     it("should be able to check the connection", () => {
         const result = context.isConnected();
@@ -25,18 +33,32 @@ describe("MongoDb test suite", function() {
         assert.deepEqual(result, expected);
     });
 
-    it('should be able to register', async ()=>{
-      const {nome, poder} = await context.create(MOCK_HEROI_CADASTRAR)
-      assert.deepEqual({nome, poder}, MOCK_HEROI_CADASTRAR)
-
+    it("should be able to register", async () => {
+        const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR);
+        assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR);
     });
 
-    it('should be able to list', async ()=>{
-      //vou extrair apenas o nome e poder do objeto da primeira posição do retorno
-      const [{nome, poder}] = await context.read({nome:MOCK_HEROI_DEFAULT.nome})
-      const result = {
-        nome, poder
-      }
-      assert.deepEqual(result, MOCK_HEROI_DEFAULT)
-    })
+    it("should be able to list", async () => {
+        //vou extrair apenas o nome e poder do objeto da primeira posição do retorno
+        const [{ nome, poder }] = await context.read({
+            nome: MOCK_HEROI_DEFAULT.nome,
+        });
+        const result = {
+            nome,
+            poder,
+        };
+        assert.deepEqual(result, MOCK_HEROI_DEFAULT);
+    });
+
+    it("should be able to update", async () => {
+        const result = await context.update(MOCK_HEROI_ID, {
+            nome: "novoNome",
+        });
+        assert.deepEqual(result.modifiedCount, 1);
+    });
+
+    it('should be able to delete', async()=>{
+      const result = await context.delete(MOCK_HEROI_ID);
+      assert.deepEqual(result.deletedCount,1)
+    });
 });
