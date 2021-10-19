88fd357072f1a54e7c192e9c7775d97d3e001b19
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-10-19 11:19:07 -0300

    create and update methods

--- 14-hapi-api-joi/src/routes/heroRoutes.js
+++ 14-hapi-api-joi/src/routes/heroRoutes.js
@@ -2,6 +2,10 @@ const Joi = require('joi')
 
 const BaseRoute = require("./base/baseRoute");
 
+const failAction = (request, headers, erro )=>{
+    throw erro
+}
+
 class HeroRoutes extends BaseRoute {
     constructor(db) {
         super();
@@ -14,9 +18,7 @@ class HeroRoutes extends BaseRoute {
             method: "GET",
             config:{
                 validate:{
-                    failAction: (request, headers, erro )=>{
-                        throw erro
-                    },
+                    failAction,
                     query:{
                         skip: Joi.number().integer().default(0),
                         limit: Joi.number().integer().default(10),
@@ -41,6 +43,80 @@ class HeroRoutes extends BaseRoute {
                 }
             },
         };
+    }
+
+    create(){
+        return{
+            path: '/herois',
+            method: 'POST',
+            config: {
+                validate:{
+                    failAction,
+                    payload:{
+                        nome: Joi.string().required().min(3).max(100),
+                        poder: Joi.string().required().min(2).max(10)
+                    }
+                }
+            },
+            handler: async (request) =>{
+                try {
+                    const {nome, poder} = request.payload
+                    const result = await this.db.create({nome, poder})
+                    
+                    return {
+                        message: 'Heroi cadastrado com sucesso',
+                        _id: result._id
+                    }
+                } catch (error) {
+                    console.log('Erro', error)
+                    return 'Internal Error'
+                }
+            }
+        }
+    }
+
+    update(){
+        return{
+            path: '/herois/{id}',
+            method: 'PATCH', 
+            config:{
+                validate:{
+                    params:{
+                        id: Joi.string().required()
+                    },
+                    payload:{
+                        nome: Joi.string().min(3).max(100),
+                        poder: Joi.string().min(2).max(100)
+                    }
+                }
+            },
+            handler: async (request)=>{
+                try {
+                    const {id} = request.params;
+
+                    const {payload} = request;
+
+                    const dadosString = JSON.stringify(payload)
+                    const dados = JSON.parse(dadosString) //todas as chaves sem valor serão removidas
+
+                    const result = await this.db.update(id, dados)
+
+                    if(result.modifiedCount === 1){
+                        return{
+                            message: 'Heroi atualizado com sucesso'
+                        }
+                    }
+                    
+                    return{
+                        message: 'Impossivel atualizar'
+                    }
+
+                } catch (error) {
+                    console.error('erro', error);
+                    return 'Erro interno'
+                }
+            }
+        }
     }
 }
 
--- 14-hapi-api-joi/src/tests/apiHeros.test.js
+++ 14-hapi-api-joi/src/tests/apiHeros.test.js
@@ -2,10 +2,28 @@ const assert = require("assert");
 const api = require("../../api");
 
 let app = {};
+const MOCK_CADASTRAR = {
+    nome: 'Chapolin',
+    poder: 'marreta'
+}
+
+const MOCK_INICIAL ={
+    nome: 'ironman',
+    poder: 'armadura'
+}
+
+let MOCK_ID = '';
 
 describe.only("Heroes Api Test Suite", function () {
     this.beforeAll(async () => {
         app = await api;
+        const result = await app.inject({
+            method: 'POST',
+            url: '/herois',
+            payload: JSON.stringify(MOCK_INICIAL)
+        })
+        const dados = JSON.parse(result.payload);
+        MOCK_ID = dados._id;
     });
     it("should be ale to list /herois", async () => {
         const result = await app.inject({
@@ -48,4 +66,59 @@ describe.only("Heroes Api Test Suite", function () {
 
 
     });
+
+    it('should register an item', async ()=>{
+        const result = await app.inject({
+            method: "POST",
+            url: `/herois`,
+            payload: MOCK_CADASTRAR
+        });
+        const statusCode = result.statusCode
+        const {message, _id} = JSON.parse(result.payload)
+
+
+        assert.ok(statusCode === 200)
+        assert.notStrictEqual(_id, undefined)
+        assert.deepEqual(message, "Heroi cadastrado com sucesso")
+    });
+
+    it('should be able to update partially (patch)', async()=>{
+        const _id = MOCK_ID;
+        const expected = {
+            poder: 'Super Mira'
+        }
+
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        })
+
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+
+        assert.ok(statusCode === 200)
+        assert.deepEqual(dados.message, 'Heroi atualizado com sucesso')
+
+    })
+
+    it('should not be able to update partially (patch)', async()=>{
+        const _id = `507f191e810c19729de860ea`;
+        const expected = {
+            poder: 'Super Mira'
+        }
+
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        })
+
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+
+        assert.ok(statusCode === 200)
+        assert.deepEqual(dados.message, 'Impossivel atualizar')
+
+    })
 });
